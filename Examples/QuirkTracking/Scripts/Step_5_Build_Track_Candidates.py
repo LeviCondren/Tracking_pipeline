4363ae542b805469843be49a3a7846b4dde48487
Levi <85909265+LeviCondren@users.noreply.github.com>
2026-06-21 13:09:22 -0700

    Fix hardcoded project_root to use dynamic path

--- Examples/QuirkTracking/Scripts/Step_5_Build_Track_Candidates.py
+++ Examples/QuirkTracking/Scripts/Step_5_Build_Track_Candidates.py
@@ -20,7 +20,7 @@ CONFIG = os.getenv('CONFIG', 'default_value4')
 
 
 # Set the project root directory
-project_root = "/global/homes/l/lcondren/pipeline_copy"
+project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(project_root)
 
 
